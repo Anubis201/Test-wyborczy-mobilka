30b88b0f3e20acf60e2719f52fad8683b71769c9
Anubis201 <anubisanubias@gmail.com>
2022-04-25 19:17:19 +0200

    ok

--- src/app/result/result-service/result.service.ts
+++ src/app/result/result-service/result.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalService } from 'src/app/services/modal/modal.service';
 import { ResultDataModel } from 'src/models/interfaces/result-data.model';
 
 @Injectable()
 export class ResultService {
   data: ResultDataModel;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private modalService: ModalService,
+  ) { }
 
   getData() {
     this.data = this.router.getCurrentNavigation().extras.state as ResultDataModel;
   }
 
   handleGoHome() {
-    // this.confirmModal('Czy, aby na pewno chcesz wrócić do listy testów? Uwaga dane zostaną utracone!').then(val => {
-    //   if (val.data.isConfirm) {
-    //     this.router.navigateByUrl('/lista');
-    //   }
-    // });
+    this.modalService.confirmModal('Czy, aby na pewno chcesz wrócić do listy testów? Dane zostaną utracone!').then(val => {
+      if (val.data.isConfirm) {
+        this.router.navigateByUrl('/lista');
+      }
+    });
   }
 }
